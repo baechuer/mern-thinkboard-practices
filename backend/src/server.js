95543c09dfc8cf9e52573bad7e33dadef748a016
Jacky Zhang <jzha9115@uni.sydney.edu.au>
2025-08-07 01:54:38 +1000

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import path from 'path';
 import { connectDB } from '../config/db.js';
 import notesRoutes from './routes/notesRoutes.js';
 import rateLimiter from './middlewares/rateLimiter.js';
@@ -9,10 +9,15 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173',
-}));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: 'http://localhost:5173',
+    }));
+}
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
@@ -20,6 +25,17 @@ app.use((req, res, next) => {
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+    // Regex catch-all (matches everything)
+    app.get(/.*/, (req, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    });
+  }
+  
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
